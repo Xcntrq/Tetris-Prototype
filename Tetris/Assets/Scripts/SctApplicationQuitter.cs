29482e1a2c9676b717fbdadd6876b44bc53234c0
Xcntrq <92533391+Xcntrq@users.noreply.github.com>
2022-01-24 16:14:10 +0300

    Minor UI improvements
    UI:
    - Changed button name from "Play Again" to "Again".
    - Changed sprites for replay button and resume button.
    - Added quit button.
    
    Scripts:
    - Added SctApplicationQuitter, guess what it does.
    
    Builds:
    - Added the first Android build.

---
+++ Tetris/Assets/Scripts/SctApplicationQuitter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace nsApplicationQuitter
+{
+    public class SctApplicationQuitter : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
